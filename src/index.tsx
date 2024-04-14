deec84450a483e5437f54cf60cb7694621bcfc81
pudgekaramelkin <whowiewas@gmail.com>
2024-04-14 11:42:23 +0300

    feat: fix after review
    fix after review

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
-import { useLocalStorage } from './components/hooks/useLocalStorage';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+import { App } from './components/app';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [appState, setAppState] = useLocalStorage<ArticleStateType>(
-		'state',
-		defaultArticleState
-	);
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': appState.fontFamilyOption.value,
-					'--font-size': appState.fontSizeOption.value,
-					'--font-color': appState.fontColor.value,
-					'--container-width': appState.contentWidth.value,
-					'--bg-color': appState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm setAppState={setAppState} />
-			<Article />
-		</div>
-	);
-};
 
 root.render(
 	<StrictMode>
